38a3b6638734eae7fbcccf544517aa515644c391
ctulhuweb <ctulhuweb@gmail.com>
2020-04-22 16:04:19 +0700

    Updates

---
+++ precache-manifest.3c8f91d715d5c1621232b435afa453e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c36f8a07d324888657c66f93d85c6082",
+    "url": "/wa-react/index.html"
+  },
+  {
+    "revision": "676a656a181b8f49ac18",
+    "url": "/wa-react/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "8a86f28eae91927abcf2",
+    "url": "/wa-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "676a656a181b8f49ac18",
+    "url": "/wa-react/static/js/2.aeb2b448.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/wa-react/static/js/2.aeb2b448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a86f28eae91927abcf2",
+    "url": "/wa-react/static/js/main.3ddfb6d1.chunk.js"
+  },
+  {
+    "revision": "03a868bac684ab7d547c",
+    "url": "/wa-react/static/js/runtime-main.533059cf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18ae3b1659b0911d5c45bbb2c027492e.js"
+  "/wa-react/precache-manifest.3c8f91d715d5c1621232b435afa453e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wa-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
